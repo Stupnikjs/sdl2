9b941b1827ab2dbed20fa38ca62e250b53eaf3ca
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-11-12 13:24:57 +0100

    Update instrument.zig

--- src/instrument.zig
+++ src/instrument.zig
@@ -12,7 +12,7 @@ pub const Instrument = enum {
     triangleWave,
 };
 
-pub fn playInstrument(buffer: []u8, offset: *f64, params: SoundParams, instrument: Instrument, allocator: std.mem.Allocator) !void {
+pub fn playMap(buffer: []u8, offset: *f64, params: SoundParams, map: PlayMap, allocator: std.mem.Allocator) !void {
     const buffer_len_float: f64 = @floatFromInt(buffer.len);
     const chunk_size_usize: usize = @intCast(params.chunk_len);
     const chunk_size_f64: f64 = @floatFromInt(chunk_size_usize);
@@ -20,8 +20,18 @@ pub fn playInstrument(buffer: []u8, offset: *f64, params: SoundParams, instrumen
 
     // last iteration to finsh buffer
     const rest: f64 = @mod(buffer_len_float, iter_num);
+
+    // inter num rouned to int 
     const iter_num_usize: usize = @intFromFloat(iter_num);
     const rest_usize: usize = @intFromFloat(rest);
+
+    // each loop allocate a chunk buffer 
+    // and copy it to the main buffer passed to sdl 
+    // last iteration is with the rest 
+    // should be able to pass instrument for each chunk 
+    // the play map should be same length as iteration if iter_num_usize != map.seq_len 
+    // so we should iter on map 
+
 
     for (0..iter_num_usize + 1) |i| {
         if (i != iter_num_usize) {
