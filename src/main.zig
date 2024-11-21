bd44d44d0cd053f92b1ad33c7c69db28621e753b
stupnikk <andre.lacassagne82@gmail.com>
2024-11-21 22:38:48 +0100

    UI SDL

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,11 @@ const types = @import("types.zig");
 const SoundParams = types.SoundParams;
 const SDL = @import("sdl.zig").SDL;
 
+const white_key_width: c_int = 50;
+const white_key_height: c_int = 200;
+const black_key_width: c_int = 50;
+const black_key_height: c_int = 200;
+
 pub fn main() !void {
     // Initialize SDL
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) != 0) {
@@ -17,8 +22,8 @@ pub fn main() !void {
         "michel",
         SDL.SDL_WINDOWPOS_CENTERED,
         SDL.SDL_WINDOWPOS_CENTERED,
-        300,
-        300,
+        900,
+        900,
         SDL.SDL_WINDOW_SHOWN,
     );
     if (Window == null) {
@@ -31,15 +36,30 @@ pub fn main() !void {
     if (renderer == null) {
         return error.RendererCreationFailed;
     }
-    defer SDL.SDL_DestroyRenderer(renderer);
 
-    // Set Render Draw Color
-    if (SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) != 0) {
-        return error.RenderDrawColorFailed;
-    }
-    // Clear and Present Renderer
+    // Set the render draw color to black (R: 0, G: 0, B: 0, A: 255) and clear the screen
+    _ = SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     _ = SDL.SDL_RenderClear(renderer);
+
+    // Set the render draw color to yellow (R: 255, G: 255, B: 0, A: 255)
+    if (SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255) != 0) {
+        return error.SetRenderColorFailed;
+    }
+
+    // Create a rectangle to draw (position x, y, width, height)
+    const rect = SDL.SDL_Rect{
+        .x = 300,
+        .y = 300,
+        .w = 300,
+        .h = 300,
+    };
+
+    // Render the yellow rectangle
+    _ = SDL.SDL_RenderFillRect(renderer, &rect);
+
+    // Present the rendered content
     SDL.SDL_RenderPresent(renderer);
+    defer SDL.SDL_DestroyRenderer(renderer);
 
     // Wait before exiting
     std.time.sleep(10 * std.time.ns_per_s);
