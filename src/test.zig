f97da5b26d49b70352ed223abe1573d2fd5e4f35
stupnikk <andre.lacassagne82@gmail.com>
2024-11-27 22:08:00 +0100

    UI SDL

--- src/test.zig
+++ src/test.zig
@@ -8,15 +8,33 @@ const wav = @import("wav.zig");
 
 test "wav writefile" {
     // Initialize SDL
+
+    // pass effect to buff A
+    //const u32_buffer_size: u32 = @intCast(buffA.len);
+    //var header = wav.WavHeader.init(u32_buffer_size);
+    //var filename = [_]u8{ 'h', 'e', '.', 'w', 'a', 'v' };
+    // try header.WriteWav(buffA, &filename);
+    // try types.bufferToCSV(buffA);
+
+}
+
+test "ui BUFFER PLOT" {
+    var buffA = [_]u8{
+        1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40,
+        59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40,
+        40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 40, 1,
+        4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 4,  40, 59, 59,
+        40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,
+        4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59,
+        59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40, 40, 1,  4,  40, 59, 59, 40,
+        40, 1,  4,  40, 59, 59, 40, 40, 1,  4,  40, 59, 59, 40, 40,
+    };
+    try ui.uiWrapper(buffA[0..]);
+}
+
+pub fn buildRandomBuf() ![]u8 {
     var Asin: types.Note = types.Note.init(types.Instrument.sinWave, 200, false);
     var seq = try Asin.PlayRange(false, std.heap.page_allocator);
     const paramsA = types.SoundParams.init(44100, 1024, std.heap.page_allocator);
-    const buffA = try sdl.buildBuffer(paramsA, seq[0..]);
-    // pass effect to buff A
-    const u32_buffer_size: u32 = @intCast(buffA.len);
-    var header = wav.WavHeader.init(u32_buffer_size);
-    var filename = [_]u8{ 'h', 'e', '.', 'w', 'a', 'v' };
-    try header.WriteWav(buffA, &filename);
-    try types.bufferToCSV(buffA);
-    try ui.uiWrapper();
+    return try sdl.buildBuffer(paramsA, seq[0..]);
 }
--- src/ui.zig
+++ src/ui.zig
@@ -35,7 +35,7 @@ pub fn getRgbFromColor(color: Color) [4]u8 {
     }
 }
 
-pub fn uiWrapper() !void {
+pub fn uiWrapper(buffer: []u8) !void {
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) != 0) {
         return error.SDLInitFailed;
     }
@@ -61,18 +61,9 @@ pub fn uiWrapper() !void {
         return error.RendererCreationFailed;
     }
 
-    _ = SDL.SDL_SetRenderDrawColor(renderer, 100, 30, 200, 255);
+    _ = SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
 
-    _ = SDL.SDL_RenderDrawLine(renderer, 0, 0, 30, 300);
-
-    const buffer = [_]u8{ 1, 3, 4, 34 };
-    for (buffer, 0..buffer.len) |b, i| {
-        const x: c_int = @intCast(i);
-        const y: c_int = @intCast(b);
-        _ = SDL.SDL_RenderDrawLine(renderer, 0, 0, x, y);
-        _ = SDL.SDL_RenderDrawPoint(renderer, x, y);
-    }
-
+    try BufferPlot(renderer.?, buffer);
     SDL.SDL_RenderPresent(renderer);
     // Set the render draw color to black (R: 0, G: 0, B: 0, A: 255) and clear the screen
 
@@ -81,4 +72,44 @@ pub fn uiWrapper() !void {
     defer SDL.SDL_DestroyRenderer(renderer);
     // Wait before exiting
     std.time.sleep(10 * std.time.ns_per_s);
+}
+
+pub fn BufferPlot(renderer: *SDL.SDL_Renderer, buffer: []u8) !void {
+    _ = buffer;
+    const Ax = renderAxes(renderer, 10);
+    _ = Ax;
+    const testBuff = [_]u8{ 10, 20, 30, 40 };
+    var last_point_x: c_int = 0;
+    var last_point_y: c_int = 0;
+    for (0..testBuff.len) |i| {
+        const c_i: c_int = @intCast(i);
+        const c_y: c_int = @intCast(testBuff[i]);
+        _ = SDL.SDL_RenderDrawLine(renderer, last_point_x, last_point_y, c_i, c_y);
+        last_point_x = c_i;
+        last_point_y = c_y;
+    }
+}
+
+const Axes = struct { origin_x: c_int, origin_y: c_int, top_y: c_int, top_x: c_int };
+pub fn renderAxes(renderer: *SDL.SDL_Renderer, n: c_int) Axes {
+    const origin_x = @divTrunc(window_width, n);
+    const top_y = @divTrunc(window_height, n);
+    const origin_y = @divTrunc(window_height * (n - 1), n);
+    const top_x = @divTrunc(window_width * (n - 1), n);
+
+    _ = SDL.SDL_RenderDrawLine(renderer, origin_x, origin_y, origin_x, top_y);
+    _ = SDL.SDL_RenderDrawLine(renderer, origin_x, origin_y, top_x, origin_y);
+    return .{
+        .origin_x = origin_x,
+        .origin_y = origin_y,
+        .top_y = top_y,
+        .top_x = top_x,
+    };
+}
+pub fn sampleExtract(slice: []u8) i16 {
+    const first = slice[0];
+    const sec = slice[1];
+    const buff: [2]u8 = [2]u8{ first, sec };
+    const sample: i16 = std.mem.bytesToValue(i16, &buff);
+    return sample;
 }
