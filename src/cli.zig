0aa55b6a69817c77577a8e86687accbb01964c5f
stupnikk <andre.lacassagne82@gmail.com>
2024-12-02 13:55:50 +0100

    UI SDL

--- src/cli.zig
+++ src/cli.zig
@@ -2,87 +2,33 @@
 const std = @import("std");
 const strequal = std.mem.eql;
 
-pub const Token = struct {
-    tokType: tokType,
-    value: []const u8,
-};
-pub const mainCommand = enum {
-    init,
-    play,
-    exit,
-    reset,
-    list,
-    help,
-    gen,
-};
-
-pub const Command = struct {
-    cmdStr:[]const u8,
-    main: ?mainCommand,
-    arg: ?std.StringHashMap([]u8),
-
-    pub fn init(cmdstr) Command {
-        return .{
-            .main = null,
-            .arg = null,
-        };
-    }
-    pub fn nextToken(self: *Command) Token {}
-};
-
 pub fn ParseUserInput(commandBuffer: []u8) !usize {
     const stdin = std.io.getStdIn().reader();
     const command = try stdin.read(commandBuffer);
     return command;
 }
 
-pub fn Parser(cmdstr: []u8, allocator: std.mem.Allocator) !Command {
-    _ = allocator;
-    // init + millisecond length
-    // allocate a buffer of length
-    // play command for listening for the sample
-    // list instrument list effect
-    // gen fill the buffer with sound
-    // exit
-    // reset reseting buffer
-    // help
-    // const command = try allocator.create(Command);
-    var command = Command.init();
-    const extracted = try extractMainCmd(cmdstr, &command);
-
-    return command;
-}
-
-pub fn extractMainCmd(cmdstr: []u8, command: *Command) !usize {
-    var list = std.ArrayList(u8).init(std.heap.page_allocator);
-    defer list.deinit();
-    for (cmdstr) |c| {
-        if (c == ' ' or c == '\n' or c == 13) break;
+pub fn splitSpace(str: []const u8, allocator: std.mem.Allocator) ![][]const u8 {
+    var list = std.ArrayList(u8).init(allocator);
+    var res_list = std.ArrayList([]const u8).init(allocator);
+    for (str) |c| {
+        if (c == '\n' or c == 13) {
+            try res_list.append(list.items);
+            list.clearAndFree();
+            break;
+        }
+        if (c == ' ' and list.items.len > 0) {
+            try res_list.append(list.items);
+            list.clearAndFree();
+            list = std.ArrayList(u8).init(allocator);
+        }
         try list.append(c);
+        std.debug.print("{s} \n", .{list.items});
     }
-    const main = list.items;
-    if (strequal(u8, main, "exit")) command.main = mainCommand.exit;
-    if (strequal(u8, main, "help")) command.main = mainCommand.help;
-    if (strequal(u8, main, "list")) command.main = mainCommand.list;
-    if (strequal(u8, main, "play")) command.main = mainCommand.play;
-    if (strequal(u8, main, "gen")) command.main = mainCommand.gen;
-    if (strequal(u8, main, "init")) command.main = mainCommand.init;
-    if (strequal(u8, main, "reset")) command.main = mainCommand.reset;
-    return main.len;
+    // list.deinit();
+    return res_list.items;
 }
 
-
-
-// extract args are strings 
-// list takes one arg instrument or effect 
+// extract args are strings
+// list takes one arg instrument or effect
 // init takes one arg that is the sample len in milliseconds
-
-
-pub fn nextArg(cmdStr:[]u8) arg { 
-   for (cmdStr) |c| {
-     
-
-
-}
-
-}
--- src/test.zig
+++ src/test.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 const sdl = @import("sdl.zig");
 const types = @import("types.zig");
-const ui = @import("ui.zig");
+const cli = @import("cli.zig");
 const testing = std.testing;
 const expect = testing.expect;
 const wav = @import("wav.zig");
 const buf = @import("buf.zig");
+
+test "split space" {
+    const allocator = std.heap.page_allocator;
+    const splited = try cli.splitSpace("mem mem mem", allocator);
+    defer allocator.free(splited);
+    std.debug.print("splited {s} \n", .{splited});
+    try (expect(splited.len == 3));
+}
